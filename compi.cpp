274ca29ae95d272456f7a10d49dfd722a013ed88
animesh <animeshchaudhri3@gmail.com>
2023-12-05 19:33:58 +0530

    first commit

---
+++ compi.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+// string operationrec(int n,int n2,string n3,int making){
+//   if(n==0){
+//     return "a";
+    
+//   }
+  
+//   operationrec( n-1,n2,n3+'+',making+1);
+//   operationrec( n-1,n2,n3+'-',making-1);
+//   operationrec( n-1,n2,n3+'*',making*1);
+//   ret
+// }
+
+int main() {
+  int n,n2;
+  cin>>n>>n2;
+  // cout<<operationrec(n,n2," ",0);
+  cout<<1;
+  return 0;
+}
